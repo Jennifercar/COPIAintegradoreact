954b2e8355b7424028d02cb8159f64b02fc43b76
Jennifercar <jennifercarrasco.lp@gmail.com>
2023-09-12 03:56:45 -0300

    agregue contador

--- src/componentes/navbar/navbarModal/ModalCont.jsx
+++ src/componentes/navbar/navbarModal/ModalCont.jsx
@@ -10,6 +10,7 @@ import {
    } from '../navbarModal/ModalStylos';
  
   import { formatoPrecio } from '../../../util/formatoPrecio';
+import Contador from '../../../util/contador/Contador';
 
 
 const ModalCont = ({cardImg,name,precio, id, quantity})=> {
@@ -25,8 +26,7 @@ return (
 
         <ContadorButtonContainerStylos>
         <ButtonStylos onClick={() => dispatch(agregarAlCarrito({cardImg,precio, name, quantity,id }))}>+</ButtonStylos>
-
-          
+          <Contador><h2>{quantity}</h2></Contador>
           <ButtonStylos onClick={() => dispatch(sacarItem(id))}>-</ButtonStylos>
           <ButtonStylos onClick={() => dispatch(vaciarCarrito(id))}>🗑</ButtonStylos>
           
